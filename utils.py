95b7d454ef81839bae9738b5523fcac93fe30a61
Samir Nait Sadi <snaitsadi@emi.u-bordeaux.fr>
2026-02-16 10:36:18 +0100

    utils

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from typing import List, Dict, Tuple
+import json
+from datetime import datetime
+
+# Alphabet des acides aminés standards (20)
+AMINO_ACIDS = 'ACDEFGHIKLMNPQRSTVWY'
+
